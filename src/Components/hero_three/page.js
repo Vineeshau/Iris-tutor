a319c1e150e8b516295bfcb2d68b711c98336b8a
Vineesh A U <vineesh.au@chordify.com>
2024-05-21 11:54:54 +0530

    landing page corrected

--- src/Components/hero_three/page.js
+++ src/Components/hero_three/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 function Page() {
   return (
@@ -10,48 +9,69 @@ function Page() {
           IrisTutor Features
         </h2>
       </div>
-      <div className="flex flex-col sm:flex-row px-10 lg:px-40">
-        <div className="flex-grow p-2  flex flex-col items-center">
-          <Image src="/course.png" width={100} height={50} alt="Image" />
-          <h3 className="text-lg font-semibold mb-2 text-center">
-            Course Roadmap
-          </h3>
-          <p className="text-gray-1000 mb-2 text-center">
+      <div className="flex flex-col sm:flex-row px-10 lg:px-40 gap-8">
+        <div className="flex-grow p-4 flex flex-col items-center text-center object-fill">
+          <img
+            className="object-cover mb-4"
+            loading="lazy"
+            alt="Course Roadmap"
+            src="/course.png"
+          />
+          <h3 className="text-lg font-semibold mb-2">Course Roadmap</h3>
+          <p className="text-gray-700 mb-4">
             Manage individual permissions using our granular privacy settings,
             and keep track of everything at a glance with high-level,
             multi-project visibility.
           </p>
-          <Link href="#" className="text-blue-500 text-center">
-          Read more
+          <div className="mt-auto">
+            <Link href="#" className="text-blue-500">
+              Read more
             </Link>
+          </div>
         </div>
 
-        <div className="flex-grow p-4 flex flex-col items-center">
-          <Image src="/visibility.png" width={100} height={50} alt="Image" />
-          <h3 className="text-lg font-semibold mb-2 text-center">
-            Get better visibility
-          </h3>
-          <p className="text-gray-1000 mb-2 text-center">
+        <div className="flex-grow p-4 flex flex-col items-center text-center object-fill">
+          <img
+            className="object-cover mb-4"
+            loading="lazy"
+            alt="Get better visibility"
+            src="/visibility.png"
+          />
+          <div className="mt-auto">
+          <h3 className="text-lg font-semibold mb-4">Get better visibility</h3>
+          <p className="text-gray-700">
             Treat your teams like pros. Empower them to work autonomously,
             without losing sight of what’s happening.
           </p>
-          <Link href="#" className="text-blue-500 text-center">
-          Read more
+          </div>
+          <div className="mt-auto">
+            <Link href="#" className="text-blue-500">
+              Read more
             </Link>
+          </div>
         </div>
 
-        <div className="flex-grow p-4 flex flex-col items-center">
-          <Image src="/centralize.png" width={100} height={50} alt="Image" />
-          <h3 className="text-lg font-semibold mb-2 text-center">
+        <div className="flex-grow p-4 flex flex-col items-center text-center object-fill">
+          <img
+            className="object-cover mb-4"
+            loading="lazy"
+            alt="Centralize all project information"
+            src="/centralize.png"
+          />
+          <div className="mt-auto">
+          <h3 className="text-lg font-semibold mb-2">
             Centralize all project information
           </h3>
-          <p className="text-gray-700 mb-2 text-center">
+          <p className="text-gray-700">
             Your teammates can zoom in to the details of every task and you can
             zoom out to manage the big picture.
           </p>
-          <Link href="#" className="text-blue-500 text-center">
-          Read more
+          </div>
+          <div className="mt-auto">
+            <Link href="#" className="text-blue-500">
+              Read more
             </Link>
+          </div>
         </div>
       </div>
       <div className="flex items-center justify-center py-20">
@@ -60,9 +80,8 @@ function Page() {
           width="600"
           height="300"
           src="iris_tutor.mp4"
-          frameborder="0"
-          allowfullscreen
-          autoplay="false" 
+          allowFullScreen
+          autoPlay={false}
         ></iframe>
       </div>
     </div>
--- src/Components/hero_two/page.js
+++ src/Components/hero_two/page.js
@@ -2,14 +2,9 @@ import Link from "next/link";
 import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import Image from "next/image";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/Components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
 import CourseData from "../../data/courses.json";
-
+ 
 function Page() {
   return (
     <div>
@@ -22,42 +17,45 @@ function Page() {
             Link to different sections of your app.
           </p>
         </div>
-        <div className="flex flex-wrap justify-center lg:justify-center gap-6 py-10">
-          <div className="flex flex-wrap justify-center lg:justify-center gap-6 w-5/6">
-            {CourseData.map((data, index) => (
-              <Card key={index} className="lg:w-[375px] bg-white shadow-2xl py-4 rounded-2xl border-none w-96 h-[550px] flex flex-col">
-                <CardHeader>
-                  <CardTitle className="flex flex-col items-center py-8">
-                    <Image
-                      src={data.imageUrl}
-                      width={224}
-                      height={224}
-                      alt={data.title}
-                      className="w-56 h-56"
-                    />
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col justify-between flex-grow px-8">
-                  <div>
-                    <div className="text-sm text-gray-500">{data.description}</div>
-                    <p className="text-2xl lg:text-xl font-bold text-black py-10">
-                      {data.title}
-                    </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-10 justify-center">
+          {CourseData.map((data, index) => (
+            <Card
+              key={index}
+              className="bg-white shadow-2xl py-4 rounded-2xl border-none w-full h-[550px] flex flex-col"
+            >
+              <CardHeader>
+                <CardTitle className="flex flex-col items-center py-8">
+                  <Image
+                    src={data.imageUrl}
+                    width={224}
+                    height={224}
+                    alt={data.title}
+                    className="w-56 h-56"
+                  />
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col justify-between flex-grow px-8">
+                <div>
+                  <div className="text-sm text-gray-500">
+                    {data.description}
                   </div>
-                  <Link href={data.link}>
-                    <div className="text-blue-700 flex items-center mt-auto">
-                      <span>GO TO</span>
-                      <FaArrowRightLong className="ml-1" />
-                    </div>
-                  </Link>
-                </CardContent>   
-              </Card>
-            ))}
-          </div>
+                  <p className="text-2xl lg:text-xl font-bold text-black py-10">
+                    {data.title}
+                  </p>
+                </div>
+                <Link href={data.link}>
+                  <div className="text-blue-700 flex items-center mt-auto">
+                    <span>GO TO</span>
+                    <FaArrowRightLong className="ml-1" />
+                  </div>
+                </Link>
+              </CardContent>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
   );
 }
-
+ 
 export default Page;
