e667f9cd899f5ab494b498c8bbdef92d36b9b34d
Vineesh A U <vineesh.au@chordify.com>
2024-06-25 11:31:03 +0530

    bug fixed

--- src/Components/navbar/page.js
+++ src/Components/navbar/page.js
@@ -65,7 +65,7 @@ const Navbar = () => {
 
         {/* Third Section: Logout Button */}
         <div>
-          <Link href="/logout">
+          <Link href="/">
             <button
               className="bg-[#3278FF] border border-white text-white px-4 py-2 rounded-lg"
               style={{ borderWidth: "2px" }}
